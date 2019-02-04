3eb8bf0ce9b1a777dc00b73ed9fb1b10803a6e4c
YunranChen <31320138+YunranChen@users.noreply.github.com>
2019-02-04 10:08:59 -0500

    Add files via upload

---
+++ lab4/lab4_blitz_alg.R
@@ -0,0 +1,58 @@
+##count number of graphs 
+tmp <- NULL
+S <- 1000
+d <- degree(karate)
+# d <- rep(3,14)
+# d <- c(5,6,rep(1,11))
+for(i in 1:S){
+	tmp2 <- graph.deg.gen(d)
+	tmp <- c(tmp,tmp2$unif.weight)
+	cat("\r",i/S)
+}
+mm <- mean(exp(tmp))
+ss <- sd(exp(tmp))/sqrt(S)
+ss/mm
+
+
+
+
+
+###algorithm 1 from Blitzstein and Diaconis
+
+graph.deg.gen <- function(d){
+	N <- length(d)
+	if(graphical.check(d)!="graphical")return("not a graphical sequence")
+	E <- matrix(ncol=2,nrow=0)
+	prob_track <- 0
+	equiv_track <- 0
+	while(any(d>0)){
+		wh <- which(d>0)
+		i <- wh[which.min(d[wh])]
+		equiv_track <- equiv_track + lfactorial(d[i])
+		while(d[i]>0){
+			i.edge.loc <- which(E==i,arr.ind=TRUE)
+			i.edges <- NULL
+			if(is.matrix(i.edge.loc))i.edges <- E[i.edge.loc[,1],][E[i.edge.loc[,1],]!=i]
+			J <- (1:N)[-unique(c(which(d==0),i,i.edges))]
+			dropJ <- NULL
+			for(j in 1:length(J)){
+				tmp <- d; tmp[c(i,J[j])] <- tmp[c(i,J[j])] - 1
+				if(graphical.check(tmp)!="graphical")dropJ <- c(dropJ,j)
+			}
+			if(length(dropJ)>0)J <- J[-dropJ]
+			j <- J
+			if(length(J)>1){j <- sample(J,1,prob=d[J]); prob_track <- prob_track + log(d[j]/sum(d[J]))}
+			E <- rbind(E,c(i,j))
+			d[c(i,j)] <- d[c(i,j)]-1
+		}
+	}	
+	list(E=E,prob=prob_track,equiv = equiv_track,unif.weight = -prob_track -equiv_track)
+}
+
+
+###function to check graphicality
+graphical.check <- function(d){
+  ##implementation of the Havel-Hakimi check  
+  
+  #write your own function here
+}
